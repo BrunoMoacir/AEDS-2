73d2e33739dc24d8bcf85aa2a306ba081c2b1434
Bruno Moacir <brunopmoacir129@gmail.com>
2025-09-03 20:56:27 -0300

    filas

--- U4 - filas/fila.java
+++ U4 - filas/fila.java
@@ -41,6 +41,78 @@ public class fila {
             system.out.print("]");
             // TELA: [7,8,9,10,11]
         }
+        // criar um metodo que retorna true se a fila estiver vazia e false caso contrario
+        boolean isVazio(){
+            return (primeiro == ultimo);
+        }
+        // criar um metodo que mostre a lista, de forma recursiva
+        void mostrarRec(int i){
+            int i = primeiro;
+            if(i != ultimo){
+                system.out.print(array[i] + " ");
+                mostrarRec((i + 1) % array.length);
+            }
+        }
+        // verificar se um determinado elemento existe na fila
+        // percorrer a fila de forma circular,do primeiro ao ultimo comparando cada elemento
+        boolean pesquisa(int elemento){
+            boolean existe = false;
+            int i = primeiro;
+
+            while(i != ultimo && !existe){
+                if(array[i] == elemento){
+                    existe = true;
+                }
+                i = (i + 1) % array.length;
+            }
+            return existe;
+        }
+        // retornar a posicao de um elemento, ex: o 1.o elemento, 2.o elemento...
+        // logica: o primeiro elemento está em array[(primeiro + 1) % tam]
+        int retornaPos(int posicao)throws Exception{
+            int tamanhoFila = (ultimo - primeiro + array.length) %array.length; // calcula o tamanho da fila
+            if(posicao < 0 || posicao >= tamanhoFila){// verifica se a posicao é valoda
+                throw new Exception("Erro");
+            }
+            int indiceFisico = (primeiro + posicao) % array.length;// formula para calcular o indice fisico no array
+            return array[indiceFisico];
+        }
+        //refazer a classe fila para que o array tenha o tamanho exato solicitado, sem o +1
+        // solução: variavel extra para controlar o estado da fila(contador)
+        class filaComContador{
+            int [] array;
+            int primeiro,ultimo;
+            int n;// contador de elementos
+            filaComContador(int tamanho){
+                array = new int[tamanho];// cria o array com o tamanho exato
+                primeiro = 0;
+                ultimo = 0;
+                n = 0; // começa com 0 elementos
+            }
+            boolean isVazio(){
+                return(n == 0);
+            }
+            void inserir(int x) throws Exception{
+                if(n >= array.length){// verifica a fila cheia com o contador
+                    throw new Exception("Erro");
+                }
+                array[ultimo] = x;
+                ultimo = (ultimo + 1) % array.length;
+                n++;
+            }
+            int remover()throws Exception{
+                if(n == 0){
+                    throw new Exception("erro");
+                }
+                int resp = array[primeiro];// guarda o valor que será retirado para mostra-lo depois
+                primeiro = (primeiro + 1) % array.length;// move o inicio da fila para a proxima posicao, de forma circular
+                // analogia: a fila do cinema deu a volta no quarteirao e esta chegando perto da porta denovo, quando a pessoa que estava na esquina(ultima posicao do quarteirao/array) é atendida, o inicio da fila para a ser a proxima pessoa do outro lado da rua(a posicao 0 do array);
+                // o novo primeiro é o proximo da lista!!
+                n--;
+                return resp;
+            }
+
+        }
 
     }
 }
