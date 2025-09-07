45ff989dcd8dc35581d1c8eb4786317aca2a5926
Bruno Moacir <brunopmoacir129@gmail.com>
2025-09-07 12:39:34 -0300

    exercicio 17

--- U4 - filas circulares/exercicio17.java
+++ U4 - filas circulares/exercicio17.java
@@ -1,6 +1,18 @@
 package U4 - filas circulares;
 
 public class exercicio17 {
-    
-    public boolean isCheia()
+    // a primeira funcao chama a de baixo, e a de baixo chama ela mesma
+    public void mostraElementos(){
+        System.out.print("[");
+        // inicia a recursao a partir da posicao primeiro
+        mostraElementosRec(primeiro);
+        System.out.print("]");
+    }
+
+    public void mostraElementosRec(int i){
+        if(i != ultimo){// caso base, o i alcança o ultimo
+            System.out.print(array[i] + " ");// imprime o elemento da posicao atual
+            mostraElementosRec((i + 1) % array.length);// chama a proxima posicao calculando de forma circular
+        }
+    }
 }
