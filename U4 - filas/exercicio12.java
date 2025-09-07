28f6615feaa9e8d8d772f7b76fa3958918d44dec
Bruno Moacir <brunopmoacir129@gmail.com>
2025-09-07 11:57:22 -0300

    exercicio 12

---
+++ U4 - filas/exercicio12.java
@@ -0,0 +1,8 @@
+public class exercicio12 {
+    // removo cada elemento do inicio da fila e coloco no topo da pilha
+    // depois removo cada elemento do topo da pilha e coloco de volta no fim da fila
+    // fila original: 10, 20,30 -> pilha auxiliar: 30, 20, 10
+    // fila final: 30,20,10
+    }
+    
+}
