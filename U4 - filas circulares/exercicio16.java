5df4e17b07ae1f4600363d87c5a98fe163c0faca
Bruno Moacir <brunopmoacir129@gmail.com>
2025-09-07 12:29:31 -0300

    exercicio 16

---
+++ U4 - filas circulares/exercicio16.java
@@ -0,0 +1,13 @@
+package U4 - filas circulares;
+
+public class exercicio16 {
+
+    public boolean isVazia(){
+        boolean vazia = false;
+        if(primeiro == ultimo){
+            vazia = true;
+        }
+        return vazia;
+    }
+    
+}
