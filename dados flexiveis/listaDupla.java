b856730e60f3f4fa543c62ee23dc96ceb3e36fd9
Bruno Moacir <brunopmoacir129@gmail.com>
2025-10-14 20:12:49 -0300

    atividades lista dupla

---
+++ dados flexiveis/listaDupla.java
@@ -0,0 +1,98 @@
+public class listaDupla {
+    class CelulaDupla{
+        public int elemento;
+        public CelulaDupla prox,ant;
+        public CelulaDupla (){
+            this(0);
+        }
+        public CelulaDupla(int x){
+            this.elemento = x;
+            this.prox = this.ant = null;
+        }
+    }
+    class ListaDupla{
+        private CelulaDupla primeiro, ultimo;
+        public ListaDupla(){
+            primeiro = new CelulaDupla();
+            ultimo = primeiro;
+        }
+    }
+
+    public void inserirInicio(int x){
+        CelulaDupla tmp = new CelulaDupla(x);
+        tmp.ant = primeiro;
+        tmp.prox = primeiro.prox;
+        primeiro.prox = tmp;
+        if(primeiro == ultimo){
+            ultimo = tmp;
+        } else{
+            tmp.prox.ant = tmp;
+        }
+        tmp = null;
+    }
+
+    public void inserirFim(int x){
+        ultimo.prox = new CelulaDupla(x);
+        ultimo.prox.ant = ultimo;
+        ultimo = ultimo.prox;
+    }
+
+    public int removerInicio() throws Exception{
+        if(primeiro == ultimo){
+            throw new Exception ("Erro");
+        }
+        CelulaDupla tmp = primeiro;
+        primeiro = primeiro.prox;
+        int elemento = primeiro.elemento;
+        tmp.prox = primeiro.ant = null;
+        tmp = null;
+        return elemento;
+    }
+    // supondo uma lista com os elementos 3,5 e 7, execute o remover no inicio
+    public int removerInicio() throws Exception{
+        if(primeiro == ultimo){
+            throw new Exception("Erro");
+        }
+        Celula tmp = primeiro.prox;
+        int elemento = tmp.elemento;
+        primeiro.prox = tmp.prox;// liga ao proximo no
+        tmp.prox.ant = primeiro;//liga diretamente ao proximo nó
+        tmp.prox = null;
+        tmp.ant - null;
+        tmp = null;
+        return elemento;
+
+    }
+
+    public int removerFim() throws Exception{
+        if(primeiro == ultimo){
+            throw new Exception("Erro");
+        }
+        int elemento = ultimo.elemento;
+        ultimo = ultimo.ant;
+        ultimo.prox.ant = null;
+        ultimo.prox = null;
+        return elemento;
+    }
+    // supondo uma lista com os elementos 3,5 e 7, execute o remover no fim
+
+    public void inserir(int x, int pos) throws Exception{
+        int tamanho = tamanho();
+        if(pos < 0 || pos > tamanho){
+            throw new Exception("erro");
+        }
+        else if(pos == 0){
+            inserirInicio(x);
+        } else if(pos == tamanho){
+            inserirFim(x);
+        } else{
+            CelulaDupla i = primeiro;
+            for(int j = 0; j < pos; j ++, i = i.prox);
+            CelulaDupla tmp = new CelulaDupla(x);
+            tmp.ant = i;
+            tmp.prox = i.prox;
+            tmp.ant.prox = tmp.prox.ant = tmp;
+            tmp = i = null;
+        }
+    }
+}
