260835563f75f5c6c1c44649c8650fe70b258818
Bruno Moacir <brunopmoacir129@gmail.com>
2025-10-21 09:44:04 -0300

    inserir arvore binaria

--- arvore binaria/main.java
+++ arvore binaria/main.java
@@ -23,4 +23,28 @@ class ArvoreBinaria{
     public ArvoreBinaria(){
         raiz = null;
     }
+
+    public void inserir(int x) throws Exception{
+        raiz = inserir(int x, No i);// chama o inserir recursivo, que começa pela raiz
+
+    }
+
+    private No inserir(int x, No i) throws Exception{
+        if(i == null){// caso base: encontrei um lugar vazio
+            i = new No(x);// criei o novo nó aqui
+        }
+        // passo recursivo: ir para a esquerda
+        else if(x < i.elemento){
+            // chamo a recursao para esquerda(i.esq)
+            i.esq = inserir(x, i.esq);
+        }
+        else if( x > i.elemento){
+            // caso o elemento seja maior, chamo a recursao para a direita
+            i.dir = inserir(x , i.dir);
+        }else{
+            // elemento ja existe
+            throw new Exception("Elemento ja existe");
+        }
+        return i;// retorna a referencia do no atual, modificado ou nao
+    }
 }
