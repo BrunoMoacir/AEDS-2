befe546d54f9eac743bfe6ab561db8df84a38a41
Bruno Moacir <brunopmoacir129@gmail.com>
2025-11-02 19:18:05 -0300

    att

---
+++ arvoreBinaria/Arvore.java
@@ -0,0 +1,26 @@
+class No{
+    public int elemento;
+    public No esq, dir;
+    int cont;
+}
+public class Arvore{
+    private No raiz;
+
+    void inserir(int x){
+        raiz = inserir(x, raiz);
+    }
+    
+    public void inserir(int x){
+        if(i == null){
+            i = new No(x);
+        } else if(x < i.elemento){
+            i.esq = inserir(x, i.esq);
+        }else if(x > i.elemento){
+            i.dir = inserir(x, i.dir);
+        } else{
+            i.cont ++;// elemento repetido -> incrementa contador
+        }
+        return i;
+
+    }
+}
